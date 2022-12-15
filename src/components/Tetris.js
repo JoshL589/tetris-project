ec672501b7f4890561d4820a4a77152e3ac6b95d
Joshua Li <joshuazli2020@gmail.com>
2022-12-15 16:04:50 +1000

    building out the hooks

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -18,18 +18,28 @@ const Tetris = () => {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
-  const [player] = usePlayer();
+  const [player, updatePlayerPos, resetPlayer] = usePlayer();
   const [stage, setStage] = useStage(player);
 
   console.log("re-render");
 
-  const movePlayer = (dir) => {};
+  const movePlayer = (dir) => {
+    updatePlayerPos({ x: dir, y: 0 });
+  };
 
-  const startGame = () => {};
+  const startGame = () => {
+    //Reset everything
+    setStage(createStage());
+    resetPlayer();
+  };
 
-  const drop = () => {};
+  const drop = () => {
+    updatePlayerPos({ x: 0, y: 1, collided: false });
+  };
 
-  const dropPlayer = () => {};
+  const dropPlayer = () => {
+    drop();
+  };
 
   const move = ({ keyCode }) => {
     if (!gameOver) {
@@ -57,7 +67,7 @@ const Tetris = () => {
               <Display text="Level" />
             </div>
           )}
-          <StartButton />
+          <StartButton onClick={startGame} />
         </aside>
       </StyledTetris>
     </StyledTetrisWrapper>
